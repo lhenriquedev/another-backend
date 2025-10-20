bcc276d94162d1823870ba6ac54093bb00c20e31
Henrique M. <lhenrique.dev@gmail.com>
2025-10-20 20:07:51 -0300

    feat: add health

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import { app } from "./app.ts";
 
 const port = Number(process.env.PORT) || 3333;
 
+app.get('/health', async () => {
+  return { status: 'ok' }
+})
+
 app.listen({ port, host: '0.0.0.0' }).then(() => {
   console.log(`HTTP server running`);
+}).catch(err => {
+  app.log.error(err)
+  process.exit(1)
 });
